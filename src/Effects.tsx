b7dbcd75cacbb054b64fcf3bafe1af6204b008da
Daniil <daniil.molokov@bk.ru>
2022-06-08 22:49:35 +0300

    Основы React

--- src/Effects.tsx
+++ src/Effects.tsx
@@ -1,5 +1,22 @@
 import { subscribe, unsubscribe } from './resources/API';
+import { useEffect, useState } from 'react';
 
 export function Effects(props: { sourceId: string }) {
-    return <div>123</div>;
+    const [message, setMessage] = useState(-1);
+
+    useEffect(() => {
+        const cb = (message: number) => {
+            setMessage(message);
+        };
+        subscribe(props.sourceId, cb);
+        return () => {
+            unsubscribe(props.sourceId, cb);
+            setMessage(-1);
+        };
+    }, [props.sourceId]);
+    return (
+        <div>
+            {props.sourceId}: {message}
+        </div>
+    );
 }
